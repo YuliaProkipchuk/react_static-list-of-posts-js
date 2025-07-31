1b2b37d205ebff86d615a2e332a165d998b3c33e
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-07-31 14:10:54 +0300

    fix bugs

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -5,7 +5,7 @@ export const PostList = ({ posts }) => {
   return (
     <div className="PostList">
       {posts.map(post => (
-        <PostInfo key={post.id} post={post} />
+        <PostInfo post={post} key={post.id} />
       ))}
     </div>
   );
